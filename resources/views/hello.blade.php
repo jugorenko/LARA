b6a3c482fbb64fdb2c4199acdf393559e7ca96ed
jugorenko <jevgenijs.ugorenko@gmail.com>
2022-05-18 16:35:46 +0300

    2. Izveidot projektam jaunu zaru (dev branch) un visas nākamās izmaiņas veikt dev zarā. git checkout -b <branch-name>

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,1 @@
+Hi to everyone!!! My name is {{ $name }}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    $name = 'jugin';
+    return view('hello', [
+        'name' => $name,
+    ]);
+});
